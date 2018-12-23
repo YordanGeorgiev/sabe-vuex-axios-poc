b480affb72f3e917fdacc4bd4c4aa8f455905e5f
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-12-23 13:32:41 +0200

    init from https://sabe.io/tutorials/getting-started-with-vuex and back-end call addition

---
+++ README.md
@@ -0,0 +1,5 @@
+#  sabe-vuex-axios-poc
+
+## 1. WHAT ?!
+This is just a small POC for a vuex app fetching data from a JSON backend with axios, based on the easiest "grasp vuex tutorial in 5 min" @: https://sabe.io/tutorials/getting-started-with-vuex ... 
+The more generic "item" has been replaced instead of "task" and the initial method to load the data from a public json api 
---
+++ app.js
@@ -0,0 +1,95 @@
+
+var state = {
+	items: []
+ };
+
+var getters = {
+	getItems: state => state.items
+}
+
+ var mutations = {
+        ADD_ITEM: (state, payload) => {
+            var newItem = {
+                id: payload.newId,
+                name: payload.name,
+                completed: false
+            }
+            state.items.unshift(newItem);
+        },
+        SET_ITEMS (state, items) {
+            state.items = items
+        },
+        TOGGLE_ITEM: (state, payload) => {
+            var item = state.items.find(todo => todo.id === payload);
+            item.completed = !item.completed;
+        },
+        DELETE_TODO: (state, payload) => {
+            var index = state.items.findIndex(todo => todo.id === payload);
+            state.items.splice(index, 1);
+        }
+ };
+
+   var actions = {
+		loadItems ({ commit }) {
+			axios
+			.get('http://echo.jsontest.com/name/this-is-item-01-hit-enter-for-more')
+			.then(r => r.data) // replace here ^^^ with your backend call
+			.then(items => {
+				commit('SET_ITEMS', Array(items)) // take out the "Array( if your data source spts arrays already ...
+			})
+		  },
+        addItem: (context, payload) => {
+            context.commit("ADD_ITEM", payload)
+        },
+        toggleItem: (context, payload) => {
+            context.commit("TOGGLE_ITEM", payload)
+        },
+        deleteItem: (context, payload) => {
+            context.commit("DELETE_TODO", payload)
+        }
+    }
+
+ var store = new Vuex.Store({
+        state: state,
+        getters: getters,
+        mutations: mutations,
+        actions: actions
+    });
+
+ Vue.component("list-items", {
+  		  created () {
+    			this.$store.dispatch('loadItems')
+  		  },
+        computed: {
+            items() {
+                return this.$store.getters.getItems;
+            }
+        },
+        methods: {
+            toggleItem: function(id) {
+                this.$store.dispatch("toggleItem", id);
+            },
+            deleteItem: function(id) {
+                this.$store.dispatch("deleteItem", id);
+            }
+        },
+        template: "#list-items"
+    });
+	
+var app = new Vue({
+        data: () => ({
+            name: "",
+            newId: 3
+        }),
+        methods: {
+            addItem: function() {
+                this.$store.dispatch("addItem", this);
+                this.newId ++;
+                this.name = "";
+            }
+        },
+        store: store,
+        el: "#app",
+        template: "#app-template"
+    });
+	
